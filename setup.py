a1b700f5c3346afc1875526a2b1ee5331f7d018b
Laurent Trinques <scorpio@qelectrotech.org>
2021-05-21 18:59:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
 
                                        #name_of_executable = folder.module:function_to_execute
     entry_points={'console_scripts': ['qet_tb_generator=src.main:main']},
-    install_requires=[PySimpleGUI],
+    install_requires=[],
     keywords='qelectrotech terminal block electric',
 
     classifiers=[
